3013c6fbb5afe3e4657e50b9ec6b3e0a8fa1b0c2
Edmund Wong <ewongbb@pw-wspx.org>
2017-10-27 12:14:01 +0800

    Cleanup: extras/parsedplatform.py

--- NetworkScanners/BwAuthority/extras/parseplatform.py
+++ NetworkScanners/BwAuthority/extras/parseplatform.py
@@ -8,21 +8,24 @@ import math
 
 import logistic
 
+
 f = open("platforms.csv", "r")
 f.readline()
 
 upgrade_table = {}
 t0 = 0
 for l in f.readlines():
-  v = l.split(",")
-  if v[5] == "NA" or int(v[5]) == 0: continue
-  up = (float(v[5])+float(v[6])+float(v[7]))/float(v[8])
-  t = time.mktime(time.strptime(v[0], "%Y-%m-%d"))
-  if t0 == 0: t0 = t
-  upgrade_table[t-t0] = up
+    v = l.split(",")
+    if v[5] == "NA" or int(v[5]) == 0:
+        continue
+    up = (float(v[5]) + float(v[6]) + float(v[7])) / float(v[8])
+    t = time.mktime(time.strptime(v[0], "%Y-%m-%d"))
+    if t0 == 0:
+        t0 = t
+    upgrade_table[t - t0] = up
 
 
-(u,s) = logistic.estimate(upgrade_table)
+(u, s) = logistic.estimate(upgrade_table)
 
-print "s="+str(s)+", u="+str(u)
-print "Estimate 50% upgrade at: "+time.ctime(t0+u)
+print "s=" + str(s) + ", u=" + str(u)
+print "Estimate 50% upgrade at: " + time.ctime(t0 + u)
