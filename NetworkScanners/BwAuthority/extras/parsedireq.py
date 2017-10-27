ee1814fe9e4d59b2f07818de7d8d409e0e469f1f
Edmund Wong <ewongbb@pw-wspx.org>
2017-10-27 12:12:55 +0800

    Cleanup: extras/parsedireq.py

--- NetworkScanners/BwAuthority/extras/parsedireq.py
+++ NetworkScanners/BwAuthority/extras/parsedireq.py
@@ -6,7 +6,7 @@
 #           more than that, since some fail and they retry
 # So v2 fetches are 6.0 times more frequent than v3 fetches.
 
-# written 
+# written
 # n-ns-reqs
 # n-v2-ns-reqs
 
@@ -17,17 +17,21 @@ import time
 import math
 import sys
 
+import logistic
+
+
 def total_countries(l):
-  reqs = 0
-  l = l.split(" ")
-  if len(l) != 2:
-   print l
-   sys.exit(1)
-  l = l[1].split(",")
-  for c in l:
-    c = c.split("=")
-    reqs += int(c[1])
-  return reqs
+    reqs = 0
+    l = l.split(" ")
+    if len(l) != 2:
+        print l
+        sys.exit(1)
+    l = l[1].split(",")
+    for c in l:
+        c = c.split("=")
+        reqs += int(c[1])
+    return reqs
+
 
 f = open("trusted-dirreq", "r")
 
@@ -39,42 +43,43 @@ upgrade_req_table = {}
 
 l = f.readline()
 while l:
-  l = l.split(" ")
-  if l[0] == "written":
-    written = time.mktime(time.strptime(l[1], "%Y-%m-%d"))
-    nsreqs = 0
-    v2reqs = 0
-    nsips = 0
-    v2ips = 0
-    l = f.readline()
-    while l and not l.startswith("ns-ips"): l = f.readline()
-    nsreqs = total_countries(l)
-    l = f.readline()
-    while l and not l.startswith("ns-v2-ips"): l = f.readline()
-    v2reqs = total_countries(l)
-    l = f.readline()
-    while l and not l.startswith("n-ns-reqs"): l = f.readline()
-    nsips = total_countries(l)
-    l = f.readline()
-    while l and not l.startswith("n-v2-ns-reqs"): l = f.readline()
-    v2ips = total_countries(l)
+    l = l.split(" ")
+    if l[0] == "written":
+        written = time.mktime(time.strptime(l[1], "%Y-%m-%d"))
+        nsreqs = 0
+        v2reqs = 0
+        nsips = 0
+        v2ips = 0
+        l = f.readline()
+        while l and not l.startswith("ns-ips"):
+            l = f.readline()
+        nsreqs = total_countries(l)
+        l = f.readline()
+        while l and not l.startswith("ns-v2-ips"):
+            l = f.readline()
+        v2reqs = total_countries(l)
+        l = f.readline()
+        while l and not l.startswith("n-ns-reqs"):
+            l = f.readline()
+        nsips = total_countries(l)
+        l = f.readline()
+        while l and not l.startswith("n-v2-ns-reqs"):
+            l = f.readline()
+        v2ips = total_countries(l)
 
-    #print "Written at "+time.ctime(written)+" v3-ip: "+str(nsips)+\
-    #      " v2-ip: "+str(v2ips)+" v3-reqs: "+str(nsreqs)+\
-    #      " v2-reqs "+str(v2reqs)
-    upgrade_ip_table[written-t0] = nsips/(nsips+(v2ips/8.0))
-    upgrade_req_table[written-t0] = nsreqs/(nsreqs+(v2reqs/8.0))
+        # print "Written at "+time.ctime(written)+" v3-ip: "+str(nsips)+\
+        #       " v2-ip: "+str(v2ips)+" v3-reqs: "+str(nsreqs)+\
+        #       " v2-reqs "+str(v2reqs)
+        upgrade_ip_table[written - t0] = nsips / (nsips + (v2ips / 8.0))
+        upgrade_req_table[written - t0] = nsreqs / (nsreqs + (v2reqs / 8.0))
 
-  l = f.readline()
-
-
-import logistic
+    l = f.readline()
 
 (u_ip, s_ip) = logistic.estimate(upgrade_ip_table)
 (u_req, s_req) = logistic.estimate(upgrade_req_table)
 
-print "s_ip="+str(s_ip)+", u_ip="+str(u_ip)
-print "Estimate 50% IP upgrade at: "+time.ctime(t0+u_ip)
+print "s_ip=" + str(s_ip) + ", u_ip=" + str(u_ip)
+print "Estimate 50% IP upgrade at: " + time.ctime(t0 + u_ip)
 
-print "s_req="+str(s_req)+", u_req="+str(u_req)
-print "Estimate 50% REQ upgrade at: "+time.ctime(t0+u_req)
+print "s_req=" + str(s_req) + ", u_req=" + str(u_req)
+print "Estimate 50% REQ upgrade at: " + time.ctime(t0 + u_req)
