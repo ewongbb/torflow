7b6036fafd5693d62a8bc0c64391574d3e0a7975
Edmund Wong <ewongbb@pw-wspx.org>
2017-10-27 12:09:47 +0800

    Cleanup: extras/logistic.py

--- NetworkScanners/BwAuthority/extras/logistic.py
+++ NetworkScanners/BwAuthority/extras/logistic.py
@@ -11,22 +11,22 @@ import math
 # So u=> Alpha; s=>Beta and x=>ln(upgrade_rate/(1-upgrade_rate))
 # and time => y
 
+
 def estimate(upgrade_table):
-  y = {}
-  for t in upgrade_table.iterkeys():
-    x = math.log(upgrade_table[t]/(1.0-upgrade_table[t]))
-    y[x] = t
+    y = {}
+    for t in upgrade_table.iterkeys():
+        x = math.log(upgrade_table[t] / (1.0 - upgrade_table[t]))
+        y[x] = t
 
-  y_ = sum(y.itervalues())/len(y)
-  x_ = sum(y.iterkeys())/len(y)
+    y_ = sum(y.itervalues()) / len(y)
+    x_ = sum(y.iterkeys()) / len(y)
 
-  xy__ = sum(map(lambda x: x*y[x], y.iterkeys()))/len(y)
+    xy__ = sum(map(lambda x: x * y[x], y.iterkeys())) / len(y)
 
-  x2_ = sum(map(lambda x: x*x, y.iterkeys()))/len(y)
+    x2_ = sum(map(lambda x: x * x, y.iterkeys())) / len(y)
 
-  s = Beta = (xy__ - x_*y_)/(x2_ - x_*x_)
+    s = Beta = (xy__ - x_ * y_)/(x2_ - x_ * x_)
 
-  u = Alpha = y_ - Beta*x_
+    u = Alpha = y_ - Beta * x_
 
-  return (u,s)
-
+    return (u, s)
